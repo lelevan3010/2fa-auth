b3a5dbdc78f462b432d4d52df3d3ac08011b95ce
Van Nguyen <93vannd@gmail.com>
2021-03-03 23:33:15 +0200

    Add authentication

---
+++ client/src/context/auth/Reducer.tsx
@@ -0,0 +1,12 @@
+export const reducer = (state: any, action: any) => {
+    const { Auth } = action.payload;
+    switch (action.type) {
+      case "LOGIN":
+        return { ...state, ...Auth };
+      case "LOGOUT":
+        return { ...state, ...Auth };
+      default:
+        return state;
+    }
+  };
+  
